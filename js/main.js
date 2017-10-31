0d5803e344794984843dd012fb835b34c27416c1
arkadash <arkadash@post.bgu.ac.il>
2017-10-31 16:18:39 +0200

    Rest request

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 const BASE_URL = "http://dev-moshe:3000/api";
 
+// Azure JSON doc
+// Id example: 3a3d74d928
+// https://docs.microsoft.com/en-us/azure/cognitive-services/video-indexer/video-indexer-output-json
 $(function() {
 
     const loadAllVideoIds = function(callback) {
@@ -25,7 +28,11 @@ $(function() {
         });
     };
 
-    // loadAllVideoIds(function(data){
-    //     alert(data);
-    // })
+    loadAllVideoIds(function(data){
+        alert(data);
+    });
+
+    loadVideoData("aa09d1fc27", function(data){
+        alert(data);
+    })
 });
